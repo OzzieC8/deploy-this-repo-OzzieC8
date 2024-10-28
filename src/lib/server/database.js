9056e3a154c97172cade362da3abd2293ebbff97
OzzieC8 <Ozziec86@gmail.com>
2024-10-28 05:14:10 -0700

    m

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,9 +1,8 @@
+import postgres from 'postgres';
+import { PGCONNECT } from '$env/static/private';
 
-import postgres from 'postgres'
+console.log("PGCONNECT in test file:", PGCONNECT);
 
-// see env variables in .env
-import {PGCONNECT} from '$env/static/private';
-
-const sql = postgres(PGCONNECT, {} )
+const sql = postgres(PGCONNECT, {});
 
 export default sql;
